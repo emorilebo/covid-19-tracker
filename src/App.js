0e6fb13bae9a43cd38d33309fab40b257f4b83b1
Godfrey Lebo <emorylebo@gmail,com>
2021-08-13 07:48:38 +0100

    Successfully communicated with the https://disease.sh/v3/covid-19/countries and pulled in countries in a dropdown

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  //https://disease.sh/v3/covid-19/countries
+
+  //USEEFFECT: Runs a piece of code based on a given condition
+
+  useEffect(
+    () => {
+      //async
+      const getCountriesData = async () => {
+        await fetch("https://disease.sh/v3/covid-19/countries")
+          .then((response) => response.json())
+          .then((data) => {
+            const countries = data.map((country) => ({
+              name: country.country,
+              value: country.countryInfo.iso2,
+            }));
+            setCountries(countries);
+          });
+      };
+      getCountriesData();
+    },
+    [
+      //If blank, the code on top will run once when the component loads and not again
+    ]
+  );
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID 19 TRACKER</h1>
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Option One</MenuItem>
-            <MenuItem value="worldwide">Option Two</MenuItem>
-            <MenuItem value="worldwide">Woooooo</MenuItem>
+            {/* Loop through all the countries and show a drop doen list of the options */}
+
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
